1b556e051ab31c5686fae59f6e8ee48844aba0c9
alpha.giel@beroe-inc.com <alpha.giel@beroe-inc.com>
2022-12-18 10:16:07 -0500

    middleware

--- server/app.js
+++ server/app.js
@@ -2,9 +2,30 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
-app.use('/',(req,res)=> {
-  res.send(`Hello world 🌍`)
+const middleware = (req,res,next) => {
+  console.log('middleware executed! ')
+  next()
+}
+
+// app.use(middleware);
+
+app.get('/about',(req,res)=> {
+  console.log('About')
+  return res.send(`About Page`)
 })
+
+app.get('/', middleware, (req,res)=> {
+  console.log('home')
+  return res.send(`Hello world 🌍`)
+})
+
+
+
+
+
+
+
+
 
 app.listen(PORT, ()=> {
   console.log(`port is served on port: ${PORT}`)
