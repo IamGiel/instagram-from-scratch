d31fa779bab6fbf3050c68a2492c3196c7be4a5d
alpha.giel@beroe-inc.com <alpha.giel@beroe-inc.com>
2022-12-20 08:00:43 -0500

    keys

--- server/keys.js
+++ server/keys.js
@@ -3,6 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-  MONGO_URI:`mongodb+srv://test:${encodeURIComponent("Test#4321!")}@cluster0.iy2o1qb.mongodb.net/?retryWrites=true&w=majority`,
-  JWT_SECRET:`thisisthetoken`
+  MONGO_URI:`mongodb+srv://test:${encodeURIComponent(`${process.env.PASSWORD}`)}@cluster0.iy2o1qb.mongodb.net/?retryWrites=true&w=majority`,
+  JWT_SECRET:process.env.JWT_SECRET
 }
